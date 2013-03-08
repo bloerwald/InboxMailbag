812097afd939a0cb4426f1b8480305b620554041
brefin <brefin@99138c13-881b-4dd7-bbf6-441fb451d3c2>
2013-03-08 00:53:11 +0000

    Updating the Chinese Localization. Thank you BNSSNB.

--- trunk/Localization/zhCN.lua
+++ trunk/Localization/zhCN.lua
@@ -7,10 +7,10 @@ if L then
 
 	L["DELETED_1"]  = "%s 从 %s |cffFF2020 将删除于 %s 后|r";
 	L["RETURNED_1"] = "%s 从 %s |cffFF2020 将返回于 %s 后|r";
-	L["DELETED_7"]  = "%s 从 %s |cffFF6020 将删除在 %d |4日:日;|r";
-	L["RETURNED_7"] = "%s 从 %s |cffFFA020 将返回在 %d |4日:日;|r";
-	L["DELETED"]    = "%s 从 %s |cff20FF20 将删除在 %d |4日:日;|r";
-	L["RETURNED"]   = "%s 从 %s |cff20FF20 将返回在 %d |4日:日;|r";
+	L["DELETED_7"]  = "%s 从 %s |cffFF6020 将删除于 %d |4日:日; 后|r";
+	L["RETURNED_7"] = "%s 从 %s |cffFFA020 将返回于 %d |4日:日; 后|r";
+	L["DELETED"]    = "%s 从 %s |cff20FF20 将删除于 %d |4日:日; 后|r";
+	L["RETURNED"]   = "%s 从 %s |cff20FF20 将返回于 %d |4日:日; 后|r";
 
 	L["TOTAL"]      = "总计讯息: %d";
 	L["TOTAL_MORE"] = "总计讯息: %d (%d)";
@@ -19,11 +19,11 @@ if L then
 	L["ADVANCED_MODE_DESC"] = "启用进阶模式。显示您信箱更多的讯息，并且能很好的挽回大笔的金钱。"
 	L["ADVANCED_MODE_CHANGED"] = function(enabled) return "|cff00ff96InboxMailbag: 进阶模式|r "..(enabled and "启用" or "关闭") end
 	
-	L["Quality Colors"] = true
-	L["QUALITY_COLOR_MODE_DESC"] = "Enable the display of item quality via the item's border."
-	L["QUALITY_COLORS_MODE_CHANGED"] = function(enabled) return "|cff00ff96InboxMailbag: Quality Colors|r "..(enabled and "enabled" or "disabled") end
+	L["Quality Colors"] = "品质着色"
+	L["QUALITY_COLOR_MODE_DESC"] = "启用可由物品的边框显示物品的品质"
+	L["QUALITY_COLORS_MODE_CHANGED"] = function(enabled) return "|cff00ff96InboxMailbag: 品质着色|r "..(enabled and "启用" or "关闭") end
 
 	L["MAIL_DEFAULT"] = "预设为邮包"
-	L["MAIL_DEFAULT_DESC"] = "启用此选项会导致信箱最初打开的是邮包，而非一般 " .. INBOX
+	L["MAIL_DEFAULT_DESC"] = "启用此选项会导致信箱最初打开的是邮包，而非一般" .. INBOX
 	L["MAIL_DEFAULT_CHANGED"] = function(enabled) return "|cff00ff96InboxMailbag: 信箱预设为|r "..(enabled and "信箱邮包" or INBOX) end
 end
--- trunk/Localization/zhTW.lua
+++ trunk/Localization/zhTW.lua
@@ -7,10 +7,10 @@ if L then
 
 	L["DELETED_1"]  = "%s 從 %s |cffFF2020 將刪除於 %s 後|r";
 	L["RETURNED_1"] = "%s 從 %s |cffFF2020 將返回於 %s 後|r";
-	L["DELETED_7"]  = "%s 從 %s |cffFF6020 將刪除於 %d |4日:日;|r";
-	L["RETURNED_7"] = "%s 從 %s |cffFFA020 將返回於 %d |4日:日;|r";
-	L["DELETED"]    = "%s 從 %s |cff20FF20 將刪除於 %d |4日:日;|r";
-	L["RETURNED"]   = "%s 從 %s |cff20FF20 將返回於 %d |4日:日;|r";
+	L["DELETED_7"]  = "%s 從 %s |cffFF6020 將刪除於 %d |4日:日; 後|r";
+	L["RETURNED_7"] = "%s 從 %s |cffFFA020 將返回於 %d |4日:日; 後|r";
+	L["DELETED"]    = "%s 從 %s |cff20FF20 將刪除於 %d |4日:日; 後|r";
+	L["RETURNED"]   = "%s 從 %s |cff20FF20 將返回於 %d |4日:日; 後|r";
 
 	L["TOTAL"]      = "總計訊息: %d";
 	L["TOTAL_MORE"] = "總計訊息: %d (%d)";
@@ -19,11 +19,11 @@ if L then
 	L["ADVANCED_MODE_DESC"] = "啟用進階模式。顯示您信箱更多的訊息，並且能很好的挽回大筆的金錢。"
 	L["ADVANCED_MODE_CHANGED"] = function(enabled) return "|cff00ff96InboxMailbag: 進階模式|r "..(enabled and "啟用" or "關閉") end
 	
-	L["Quality Colors"] = true
-	L["QUALITY_COLOR_MODE_DESC"] = "Enable the display of item quality via the item's border."
-	L["QUALITY_COLORS_MODE_CHANGED"] = function(enabled) return "|cff00ff96InboxMailbag: Quality Colors|r "..(enabled and "enabled" or "disabled") end
+	L["Quality Colors"] = "品質著色"
+	L["QUALITY_COLOR_MODE_DESC"] = "啟用可由物品的邊框顯示物品的品質"
+	L["QUALITY_COLORS_MODE_CHANGED"] = function(enabled) return "|cff00ff96InboxMailbag: 品質著色|r "..(enabled and "啟用" or "關閉") end
 
 	L["MAIL_DEFAULT"] = "預設為郵包"
-	L["MAIL_DEFAULT_DESC"] = "啟用此選項會導致信箱最初打開的是郵包，而非一般 " .. INBOX
+	L["MAIL_DEFAULT_DESC"] = "啟用此選項會導致信箱最初打開的是郵包，而非一般" .. INBOX
 	L["MAIL_DEFAULT_CHANGED"] = function(enabled) return "|cff00ff96InboxMailbag: 信箱預設為|r "..(enabled and "信箱郵包" or INBOX) end
 end
