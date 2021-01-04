217fd096037e16078ba20c3ba8dee66f3325c201
Bernd Lörwald <bloerwald@googlemail.com>
2021-01-04 14:36:16 +0100

    localization: add new deDE strings

--- Localization/deDE.lua
+++ Localization/deDE.lua
@@ -2,12 +2,11 @@ local L = LibStub("AceLocale-3.0"):NewLocale("InboxMailbag", "deDE", false)
 
 if L then
 --@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="concat")@
-
 --@debug@
 L["Advanced"] = "Erweitert" -- Needs review
 L["ADVANCED_MODE_DESC"] = "Aktiviere erweiterten Modus. Zeigt mehr Informationen zu deinem Briefkasten und erlaubt die Entnahme von Goldstapeln." -- Needs review
--- L["ADVANCED_MODE_DISABLED"] = ""
--- L["ADVANCED_MODE_ENABLED"] = ""
+L["ADVANCED_MODE_DISABLED"] = "|cff00ff96Posteingangstasche: Erweiterter Modus|r deaktiviert." -- Needs review
+L["ADVANCED_MODE_ENABLED"] = "|cff00ff96Posteingangstasche: Erweiterter Modus|r aktiviert." -- Needs review
 L["BAGNAME"] = "Tasche" -- Needs review
 -- L["DELETED"] = ""
 -- L["DELETED_1"] = ""
@@ -20,12 +19,12 @@ L["MAIL_DEFAULT_DESC"] = "Aktivierung veranlasst das standardmäßige öffnen de
 -- L["MAIL_DEFAULT_ENABLED"] = ""
 L["QUALITY_COLOR_MODE_DESC"] = "Aktiviert die Färbung des Rahmens anhand der Itemqualität." -- Needs review
 L["Quality Colors"] = "Qualitätsfarben" -- Needs review
--- L["QUALITY_COLORS_MODE_DISABLED"] = ""
--- L["QUALITY_COLORS_MODE_ENABLED"] = ""
+L["QUALITY_COLORS_MODE_DISABLED"] = "|cff00ff96Posteingangstasche: Rahmenfärbung|r deaktiviert." -- Needs review
+L["QUALITY_COLORS_MODE_ENABLED"] = "|cff00ff96Posteingangstasche: Rahmenfärbung|r aktiviert." -- Needs review
 -- L["RETURNED"] = ""
 -- L["RETURNED_1"] = ""
 -- L["RETURNED_7"] = ""
--- L["TOTAL"] = ""
--- L["TOTAL_MORE"] = ""
+L["TOTAL"] = "Nachrichten insgesamt: %d" -- Needs review
+L["TOTAL_MORE"] = "Nachrichten insgesamt: %d (%d)" -- Needs review
 --@end-debug@
 end
